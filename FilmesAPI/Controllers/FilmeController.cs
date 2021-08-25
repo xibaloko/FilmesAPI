29da837a05c6f2f25a41288f9e656773a44a7343
Ulisses <ulissesmilitao@hotmail.com>
2021-08-24 23:20:03 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,47 @@
+﻿using FilmesAPI.Data;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private FilmesContext _context;
+
+        public FilmeController(FilmesContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult AdicionarFilme([FromBody] Filme filme)
+        {
+            _context.Add(filme);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetFilme), new {id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult GetFilmes()
+        {
+            return Ok(_context.Filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetFilme(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(x => x.Id == id);
+
+            if (filme != null)
+            {
+                return Ok(filme);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ FilmesAPI/Data/FilmesContext.cs
@@ -0,0 +1,15 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.Data
+{
+    public class FilmesContext : DbContext
+    {
+        public FilmesContext(DbContextOptions<FilmesContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+    }
+}
